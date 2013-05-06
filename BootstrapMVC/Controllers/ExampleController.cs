4d70105d5dc42a4d9e791438b33e50d1c13d5a69
Patrik Akselsson <patrik.akselsson@valtech.se>
2013-05-06 16:59:27 +0200

    Added example code that uses adlibris commands

--- BootstrapMVC/Controllers/ExampleController.cs
+++ BootstrapMVC/Controllers/ExampleController.cs
@@ -1,12 +1,23 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using AdS.Commands;
+using AdS.Commands.Campaign;
+using AdS.Commands.Search;
 
 namespace BootstrapMVC.Controllers
 {
     public class ExampleController : Controller
     {
-        public ActionResult Index()
+        //Note: This is example code, in production you probably 
+        //want inject the command handler with an IOC container
+        private readonly CommandHandler _commandhandler = 
+            new CommandHandler(() => new HttpClientConfiguration(new Uri("http://devservices.adnet.adlibris.se", UriKind.Absolute)));
+
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var result = await _commandhandler.Execute(new GetCampaignsCommand());
+            return View(result.Result);
         }
     }
 }
